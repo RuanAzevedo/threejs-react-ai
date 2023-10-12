a50dc9d7a21931f1ec842a11069ffb210e4eb969
Ruan Azevedo <ruan.azevedo@gmail.com>
2023-10-12 07:29:10 -0400

    Update Shirt component

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -3,12 +3,13 @@ import { Environment, Center } from '@react-three/drei'
 
 import Shirt from './Shirt'
 import CameraRig from './CameraRig'
+import Backdrop from './Backdrop'
 // import Backdrop from './Backdrop'
 // import CameraRig from './CameraRig'
 
 const CanvasModel = () => {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 0] }}>
+    <Canvas shadows camera={{ position: [0, 0, 0], fov: 25 }}>
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
 
