99c10e1cbc51bc4cbcb53d93e899b0961a1d7604
Ruan Azevedo <ruan.azevedo@gmail.com>
2023-10-12 09:38:34 -0400

    Refactoring Tab component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
-import Home from './pages/Home'
-import Customizer from './pages/Customizer'
 import Canvas from './canvas'
+import Customizer from './pages/Customizer'
+import Home from './pages/Home'
 
 function App() {
   return (
     <main className="app transition-all ease-in">
       <Home />
-      <Customizer />
       <Canvas />
+      <Customizer />
     </main>
   )
 }
